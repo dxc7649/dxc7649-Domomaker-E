3efe6ade3026aca57286358bedc69d99d6a1278b
dxc7649 <42787527+dxc7649@users.noreply.github.com>
2021-04-17 23:06:01 -0400

    Domomaker E

---
+++ server/controllers/Domo.js
@@ -0,0 +1,105 @@
+const models = require('../models');
+
+const {
+  Domo,
+} = models;
+
+const makerPage = (req, res) => {
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({
+        error: 'An error occurred',
+      });
+    }
+
+    return res.render('app', {
+      csrfToken: req.csrfToken(),
+      domos: docs,
+    });
+  });
+};
+
+const makeDomo = (req, res) => {
+  if (!req.body.name || !req.body.age || !req.body.level) {
+    return res.status(400).json({
+      error: 'RAWR! All three fields: name, age and level are required',
+    });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    level: req.body.level,
+    owner: req.session.account._id,
+  };
+
+  const newDomo = new Domo.DomoModel(domoData);
+
+  const domoPromise = newDomo.save();
+
+  domoPromise.then(() => res.json({
+    redirect: '/maker',
+  }));
+
+  domoPromise.catch((err) => {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({
+        error: 'Domo already exists.',
+      });
+    }
+
+    return res.status(400).json({
+      error: 'An error occurred',
+    });
+  });
+
+  return domoPromise;
+};
+
+const getDomos = (request, response) => {
+  const req = request;
+  const res = response;
+
+  return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({
+        error: 'An error occurred',
+      });
+    }
+
+    return res.json({
+      domos: docs,
+    });
+  });
+};
+
+const deleteDomo = (req, res) => Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+  if (err) {
+    console.log(err);
+    return res.status(400).json({
+      error: 'An error occurred',
+    });
+  }
+  Domo.DomoModel.deleteMany({
+    owner: req.session.account._id,
+  }, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('successfully delete');
+    }
+  });
+
+
+  return res.json({
+    domos: docs,
+  });
+});
+
+module.exports.makerPage = makerPage;
+module.exports.getDomos = getDomos;
+module.exports.make = makeDomo;
+module.exports.deleteDomo = deleteDomo;
